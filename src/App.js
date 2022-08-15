99349f0f9e736081ddff63143a7a4c1f007b4265
anna0625 <anna860625@gmail.com>
2022-08-15 13:20:52 +1000

    pages setup and components setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import NotFound from "./pages/NotFound";
+import Navbar from "./componets/layout/Navbar";
+import Footer from "./componets/layout/Footer";
 
 function App() {
   return (
-    <div>
-      <h1>Hello!</h1>
-    </div>
+    <Router>
+      <div className="flex flex-col justify-between h-screen">
+        <Navbar />
+        <main className="container mx-auto pb-12">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="about" element={<About />} />
+            <Route path="*" element={<NotFound />} />
+            <Route path="/notfound" element={<NotFound />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
