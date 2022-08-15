8373bec055e54c22b5d62147c020c3305b269629
anna0625 <anna860625@gmail.com>
2022-08-15 17:07:43 +1000

    change useState to Reducers & useReducer Hook

--- src/App.js
+++ src/App.js
@@ -5,23 +5,26 @@ import About from "./pages/About";
 import NotFound from "./pages/NotFound";
 import Navbar from "./componets/layout/Navbar";
 import Footer from "./componets/layout/Footer";
+import { GithubProvider } from "./context/github/GithubContext";
 
 function App() {
   return (
-    <Router>
-      <div className="flex flex-col justify-between h-screen">
-        <Navbar />
-        <main className="container mx-auto pb-12">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="about" element={<About />} />
-            <Route path="*" element={<NotFound />} />
-            <Route path="/notfound" element={<NotFound />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
-    </Router>
+    <GithubProvider>
+      <Router>
+        <div className="flex flex-col justify-between h-screen">
+          <Navbar />
+          <main className="container mx-auto pb-12">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="about" element={<About />} />
+              <Route path="*" element={<NotFound />} />
+              <Route path="/notfound" element={<NotFound />} />
+            </Routes>
+          </main>
+          <Footer />
+        </div>
+      </Router>
+    </GithubProvider>
   );
 }
 
