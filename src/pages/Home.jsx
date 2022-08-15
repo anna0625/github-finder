324d1edf0548530fedffe8da546e255d5e97e760
anna0625 <anna860625@gmail.com>
2022-08-15 17:41:25 +1000

    added clear users, search users, searchUsears in Context

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import UserResults from "../componets/users/UserResults";
+import UserSearch from "../componets/users/UserSearch";
 
 export default function Home() {
   return (
     <div>
-      {/* Search Compoent */}
+      <UserSearch />
       <UserResults />
     </div>
   );
