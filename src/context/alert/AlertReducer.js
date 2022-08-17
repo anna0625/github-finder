aa7b32ea0db359452db702f3394e49c20ac30c5a
anna0625 <anna860625@gmail.com>
2022-08-17 13:48:09 +1000

    created AlerContext and Ruducer

--- src/context/alert/AlertReducer.js
+++ src/context/alert/AlertReducer.js
@@ -1,3 +1,12 @@
-const alertReducer = (state, action) => {};
+const alertReducer = (state, action) => {
+  switch (action.type) {
+    case "SET_ALERT":
+      return action.payload;
+    case "REMOVE_ALERT":
+      return null;
+    default:
+      return state;
+  }
+};
 
 export default alertReducer;
