f9c6292725989d6380bc66da3baec34e4cc061c1
anna0625 <anna860625@gmail.com>
2022-08-17 18:09:01 +1000

    remove axios

--- src/context/github/GithubReducer.js
+++ src/context/github/GithubReducer.js
@@ -6,25 +6,25 @@ const githubReducer = (state, action) => {
         users: action.payload,
         loading: false,
       };
-    // case "GET_USER":
-    //   return {
-    //     ...state,
-    //     user: action.payload,
-    //     loading: false,
-    //   };
-    // case "GET_USER_REPOS":
-    //   return {
-    //     ...state,
-    //     repos: action.payload,
-    //     loading: false,
-    //   };
-    case "GET_USER_AND_REPOS":
+    case "GET_USER":
       return {
         ...state,
-        user: action.payload.user,
-        repos: action.payload.repos,
+        user: action.payload,
         loading: false,
       };
+    case "GET_USER_REPOS":
+      return {
+        ...state,
+        repos: action.payload,
+        loading: false,
+      };
+    // case "GET_USER_AND_REPOS":
+    //   return {
+    //     ...state,
+    //     user: action.payload.user,
+    //     repos: action.payload.repos,
+    //     loading: false,
+    //   };
     case "SET_LOADING":
       return {
         ...state,
