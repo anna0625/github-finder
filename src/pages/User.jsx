ad9102d85a95e8e94d679fb346b27e9a8f218051
anna0625 <anna860625@gmail.com>
2022-08-17 16:11:39 +1000

    added four status in User Profile page

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -18,6 +18,7 @@ export default function User() {
     bio,
     blog,
     twitter_username,
+    email,
     login,
     html_url,
     followers,
@@ -51,7 +52,7 @@ export default function User() {
               </figure>
               <div className="card-body justify-end">
                 <h2 className="card-title mb-0">{name}</h2>
-                <p className="">{login}</p>
+                <p className="flex-grow-0">{login}</p>
               </div>
             </div>
           </div>
@@ -70,11 +71,87 @@ export default function User() {
                   href={html_url}
                   target="_blank"
                   rel="noreferrer"
-                  className="btn btn-outline"
+                  className="btn btn-outline hover:flex-1"
                 >
                   Visit Github Profile
                 </a>
               </div>
+            </div>
+            <div className="w-full rounded-lg shadow-md bg-base-100 stats">
+              {location && (
+                <div className="stat">
+                  <div className="stat-title text-md">Location</div>
+                  <div className="text-lg stat-value">{location}</div>
+                </div>
+              )}
+              {blog && (
+                <div className="stat">
+                  <div className="stat-title text-md">Website</div>
+                  <div className="text-lg stat-value">
+                    <a
+                      href={`https://${blog}`}
+                      className=""
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      {blog}
+                    </a>
+                  </div>
+                </div>
+              )}
+              {email && (
+                <div className="stat">
+                  <div className="stat-title text-md">Email</div>
+                  <div className="text-lg stat-value">
+                    <a
+                      href={`mailto: ${email}`}
+                      className=""
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      {email}
+                    </a>
+                  </div>
+                </div>
+              )}
+            </div>
+          </div>
+        </div>
+        <div className="w-full py-5 mb-6 rounded-lg shadow-md bg-base-100 stats">
+          <div className="stat">
+            <div className="stat-figure text-secondary">
+              <FaUsers className="text-3xlmd:text-5xl" />
+            </div>
+            <div className="stat-title pr-5">Followers</div>
+            <div className="stat-value pr-5 text-3xl md:text-4xl">
+              {followers}
+            </div>
+          </div>
+          <div className="stat">
+            <div className="stat-figure text-secondary">
+              <FaUserFriends className="text-3xlmd:text-5xl" />
+            </div>
+            <div className="stat-title pr-5">Following</div>
+            <div className="stat-value pr-5 text-3xl md:text-4xl">
+              {following}
+            </div>
+          </div>
+          <div className="stat">
+            <div className="stat-figure text-secondary">
+              <FaCodepen className="text-3xlmd:text-5xl" />
+            </div>
+            <div className="stat-title pr-5">Public Repos</div>
+            <div className="stat-value pr-5 text-3xl md:text-4xl">
+              {public_repos}
+            </div>
+          </div>
+          <div className="stat">
+            <div className="stat-figure text-secondary">
+              <FaStore className="text-3xlmd:text-5xl" />
+            </div>
+            <div className="stat-title pr-5">Public Gists</div>
+            <div className="stat-value pr-5 text-3xl md:text-4xl">
+              {public_gists}
             </div>
           </div>
         </div>
