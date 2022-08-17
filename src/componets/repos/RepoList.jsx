a2e61643c26e9243b4962f2cb22e73b71df29647
anna0625 <anna860625@gmail.com>
2022-08-17 16:44:17 +1000

    styling RepoItem component

--- src/componets/repos/RepoList.jsx
+++ src/componets/repos/RepoList.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
+import RepoItem from "./RepoItem";
 
 RepoList.propTypes = {
   repos: PropTypes.array.isRequired,
@@ -11,7 +12,7 @@ export default function RepoList({ repos }) {
       <div className="card-body">
         <h2 className="text-3xl my-4 font-old card-title">Top Repositories</h2>
         {repos.map((repo) => (
-          <h3>{repo.name}</h3>
+          <RepoItem key={repo.id} repo={repo} />
         ))}
       </div>
     </div>
